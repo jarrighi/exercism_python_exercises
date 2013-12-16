0b38f5e179ec00dd58d8fe521e2725a9dbe90ca8
Jules Arrighi <juliana.arrighi@gmail.com>
2013-12-16 00:28:27 -0800

    Refactored DNA class with DNA-RNA dictionary and exception handling

--- rna-transcription/dna.py
+++ rna-transcription/dna.py
@@ -1,19 +1,16 @@
 class DNA:
   def __init__(self, strand):
     self.dna_strand = strand
+    self.rna_bp_for_dna_bp = {'G': 'G', 'C':'C', 'A': 'A', 'T': 'U'}
 
   def to_rna(self):
     rna_strand = ''
 
     for bp in self.dna_strand:
-      if bp == 'T':
-        rna_strand += 'U'
+      try: 
+        rna_strand += self.rna_bp_for_dna_bp[bp]
 
-      elif bp in 'GCA':
-        rna_strand += bp
-
-      else:
-        print "Transacription Failed: Invalid Polymer"
-        return None
+      except KeyError:
+        print "Invalid Polymer"
 
     return rna_strand
