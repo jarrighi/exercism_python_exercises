02e6ae67e3fb5cbb2521bcde2d0c32f0a7a999d9
Jules Arrighi <juliana.arrighi@gmail.com>
2013-12-16 23:53:40 -0800

    Add to key lookup exception logic and adjust formatting.

--- rna-transcription/dna.py
+++ rna-transcription/dna.py
@@ -1,16 +1,14 @@
 class DNA:
   def __init__(self, strand):
     self.dna_strand = strand
-    self.rna_bp_for_dna_bp = {'G': 'G', 'C':'C', 'A': 'A', 'T': 'U'}
+    self.dna_to_rna = {'G': 'G', 'C':'C', 'A': 'A', 'T': 'U'}
 
   def to_rna(self):
     rna_strand = ''
-
     for bp in self.dna_strand:
       try: 
-        rna_strand += self.rna_bp_for_dna_bp[bp]
-
+        rna_strand += self.dna_to_rna[bp]
       except KeyError:
-        print "Invalid Polymer"
-
+        print "Error: Invalid Polymer"
+        return
     return rna_strand
